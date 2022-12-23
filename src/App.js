024d6bd0299f5c59943e2709769ca052b8c1b735
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2022-12-23 16:20:43 -0300

    update index.js
    adicionado css do filter

--- src/App.js
+++ src/App.js
@@ -5,23 +5,52 @@ import Login from './components/Login';
 import Header from './components/Header';
 import Form from './components/Form';
 import TotalMoney from './components/Totalmoney';
+import Filters from './components/Filters';
 
 function App() {
   const [isLogin, setLogin] = useState(true);
-  const [ listTransactions, setListTransactions] = useState([])
+  const [listTransactions, setListTransactions] = useState([])
+  const [transactionsSearch, setTransactionsSearch] = useState([]);
+  const [buttonFilter, setButtonFilter] = useState("todos");
+
+  
+  function handleSearch(filter) {
+    //realiza a filtragem conforme parametro
+    const newFilter = listTransactions.filter((transaction) => transaction.type === filter);
+
+    //seta o a variavel que recebeu a atribuição ao state
+    setTransactionsSearch(newFilter);
+    setButtonFilter(filter);
+  }
+
+  function clearSearch() {
+    //limpa e seta no status filtro o parametro todos
+    setTransactionsSearch([]);
+    setButtonFilter("todos");
+  }
+
   return (
     <>
       {
         isLogin ? (
           <>
             <Header/>
+            
             <Form
               listTransactions={listTransactions}
               setListTransactions={setListTransactions}
             />
+            
             <TotalMoney
-            listTransactions={listTransactions}
+              listTransactions={listTransactions}
             />
+
+            <Filters
+              handleSearch={handleSearch}
+              clearSearch={clearSearch}
+              buttonFilter={buttonFilter}
+            />
+            
           </>
         ) :
         (
---
+++ src/components/Filters/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './style.css'
+
+const Filters = ({ handleSearch, clearSearch, buttonFilter }) => {
+  return (
+    <div className='filters-background'>
+        <div className="container">
+            <div className="container-nav">
+                <h3>Resumo financeiro</h3>
+                <div className="container-button">
+                    <button
+                        onClick={() => clearSearch()}
+                        disabled={buttonFilter === "todos" ? true : false}
+                    >Todos
+                    </button>
+
+                    <button
+                        onClick={() => handleSearch('entrada')}
+                        disabled={buttonFilter === "entrada" ? true : false}
+                    >
+                        Entradas
+                    </button>
+                        
+                    <button
+                        onClick={() => handleSearch('entrada')}
+                        disabled={buttonFilter === "entrada" ? true : false}
+                    >
+                        Despesas
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Filters
