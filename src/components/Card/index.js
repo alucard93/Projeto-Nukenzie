c29677e34f2078480f2351665d70af918e3da69b
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2022-12-23 21:09:41 -0300

    update index.js
    correção da estilização de Form e Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,16 +6,17 @@ const Card = ({ transaction, removeTransaction }) => {
   return (
     <div className="card-background">
         <div className="container">
-            <div 
-                className={
-                    transaction.type === "Entrada"
-                    ? "container-salary"
-                    : "container-salary-exit"
+            <div className={
+                    transaction.type === "entrada"
+                    ? 
+                    "container-salary"
+                    : 
+                    "container-salary-exit"
                 }
                 >
                 <h3 className="container-salary__tittle"> {transaction.description} </h3>
                 <p className="container-salary__entry">{transaction.type}</p>
-                <p className="container__salary">{Math.abs(transaction.value)}</p>
+                <p className="container__salary">R$ {Math.abs(transaction.value)},00</p>
                 <button className="btn-delete" onClick={() => removeTransaction(transaction)}>
                     <FaTrash/>
                 </button>
